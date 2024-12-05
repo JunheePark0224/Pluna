22b63e4f81b05047570040b092d4bc0d3b41543a
Inyong Hwang <hwanginyong@seoultech.ac.kr>
2024-12-06 04:04:13 +0900

    Second try

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import firstImage from "./assets/firstimage.png";
-import secondImage from "./assets/secondimage.png";
+
 
 function App() {
   const [showSecondPage, setShowSecondPage] = useState(false);
