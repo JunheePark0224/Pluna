5111fe01a9169e43a2f812f4a22392d05a956788
Jaeuk Kwon <snust21itm@gmail.com>
2024-12-08 02:58:09 +0900

    Signup page created

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,12 +1,52 @@
 import React from "react";
+import "./SignUp.css";
+import initialLogo from "./assets/initiallogo.png"; // 로고
+import spaceBackground from "./assets/space.png"; // 배경 이미지
 
-function SignUp() {
+const SignUp = () => {
   return (
-    <div>
-      <h1>Sign Up Page</h1>
-      <p>Welcome to the Sign Up page. Create your account here!</p>
+    <div className="signup-container">
+      <img src={spaceBackground} alt="Background" className="signup-background" />
+      <div className="signup-content">
+        <img src={initialLogo} alt="Logo" className="signup-logo" />
+        <h1>Create Your Account</h1>
+        <p>Sign up to get started!</p> {/* 텍스트 수정 */}
+        <form className="signup-form">
+          <input
+            type="text"
+            placeholder="Username"
+            className="signup-input"
+          />
+          <input
+            type="text"
+            placeholder="ID"
+            className="signup-input"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="signup-input"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="signup-input"
+          />
+          <input
+            type="tel"
+            placeholder="Phone Number"
+            className="signup-input"
+          />
+          <button type="submit" className="signup-button">
+            Sign Up
+          </button>
+        </form>
+        <p className="signup-footer">
+          Already have an account? <a href="/login">Log In</a>
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default SignUp;
